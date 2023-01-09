85f81fedd3dbf40b4fc8df07d0d4858db502a159
Shubham Kumar <87716682+SK-0911@users.noreply.github.com>
2023-01-09 11:15:23 +0000

    Button

--- Data/homepage-script.js
+++ Data/homepage-script.js
@@ -1,11 +1,35 @@
-var prevScrollpos = window.pageYOffset;
+// var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos >= currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-150px";
-  }
-  prevScrollpos = currentScrollPos;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos >= currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-150px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+var d = document;
+var wrap = d.querySelector('#EventDetailsContainer');  // Initially wrap
+var items = d.querySelector('.EventDetailsBox');  // Initially itmes
+var itemCount = d.querySelectorAll('#EventDetailsContainer').length;
+var scroller = d.querySelector('.scroller');
+var pos = 0;
+var transform = Modernizr.prefixed('transform');
+
+function setTransform() {
+  items.style[transform] = 'translateX(500px)';
 }
+
+function prev() {
+  pos = Math.max(pos - 1, 0);
+  setTransform();
+}
+
+function next() {
+  pos = Math.min(pos + 1, itemCount - 1);
+  setTransform();
+}
+
+window.addEventListener('resize', setTransform);
