a045b8df437f138d391e5dc2cf61703af7ec74f4
Mayur Sharma <matrix4reloaded2000@gmail.com>
2023-01-10 12:28:59 +0530

    Buttons working

--- Data/homepage-script.js
+++ Data/homepage-script.js
@@ -1,10 +1,24 @@
- var prevScrollpos = window.pageYOffset
- window.onscroll = function() {
-   var currentScrollPos = window.pageYOffset;
-   if (prevScrollpos >= currentScrollPos) {
-     document.getElementById("navbar").style.top = "0";
-   } else {
-     document.getElementById("navbar").style.top = "-150px";
-   }
-   prevScrollpos = currentScrollPos;
- }
+var prevScrollpos = window.pageYOffset
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos >= currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+var events_scroll = 0;
+function prev_slide(){
+    events_scroll = Math.max(events_scroll-20, 0);
+    document.getElementById("EventDetailsContainer").style.transform = "translateX(" + -events_scroll + "%)";
+    console.log("preivious", "translateX(" + -events_scroll + "%)")
+
+}
+
+function next_slide() {
+    events_scroll = Math.min(events_scroll+20, 80);
+    document.getElementById("EventDetailsContainer").style.transform = "translateX(" + -events_scroll + "%)";
+    console.log("preivious", "translateX(" + -events_scroll + "%)")
+}
